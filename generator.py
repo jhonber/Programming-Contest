296062aa9bae7010ffd48515e659b1e825d847e0
Jhon Jimenez <jhonb3r@gmail.com>
2014-05-21 09:59:56 -0700

    Adding generator to hacks

---
+++ generator.py
@@ -0,0 +1,23 @@
+import sys
+import math
+from fractions import Fraction
+
+si = sys.stdin
+so = sys.stdout
+se = sys.stderr
+
+readLine = raw_input
+readArgs = lambda : readLine().split()
+readInts = lambda : map(int, readArgs())
+readInt = lambda : readInts()[0]
+
+print '|+' + ('|' * 99) + '=' + ('|'*100)
+
+#print '|+' + ('|' * 99) + '=' + ('|'*10)
+
+
+'''n = readInt()
+nums = readInts()
+for i in nums:
+    print i
+'''
