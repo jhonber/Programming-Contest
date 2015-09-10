6f61bb31b3a91282ffc4b9a4b977ecd9281074fe
Jhon Jimenez <jhonb3r@gmail.com>
2015-09-09 21:18:10 -0500

    Adding submissions

---
+++ codeforces/Codeforces Round #114 (Div. 2)/A.cpp
@@ -0,0 +1,29 @@
+// http://codeforces.com/contest/168/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define __ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define foreach(it, x) for (__typeof (x).begin() it = (x).begin(); it != (x).end(); ++it)
+#define all(x) x.begin(),x.end()
+#define D(x) cout << #x " = " << (x) << endl;
+
+template <class T> string toStr(const T &x)
+{ stringstream s; s << x; return s.str(); }
+
+template <class T> int toInt(const T &x)
+{ stringstream s; s << x; int r; s >> r; return r; }
+
+int dx[8] = {-1,-1,-1,0,1,1, 1, 0};
+int dy[8] = {-1, 0, 1,1,1,0,-1,-1};
+
+int main() {
+  int n,x,y;
+  cin >> n >> x >> y;
+
+  int target = ceil((n * y) / 100.0);
+  if (target > x) cout << target - x;
+  else cout << 0;
+
+  return 0;
+}
