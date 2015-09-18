c8180b5365f12ded05db9359bab451d5335eb5d0
Jhon Jimenez <jhonb3r@gmail.com>
2015-09-17 23:09:24 -0500

    Adding submissions

---
+++ codeforces/Codeforces Round #320 (Div. 2) [Bayan Thanks-Round]/A.cpp
@@ -0,0 +1,12 @@
+// http://codeforces.com/contest/579/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  cout << __builtin_popcount(x);
+
+  return 0;
+}
