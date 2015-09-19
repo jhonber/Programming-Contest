ea735730bb840e44644636123a5f5431095c6382
Jhon Jimenez <jhonb3r@gmail.com>
2015-09-19 09:21:48 -0500

    Adding submissions

---
+++ codeforces/Codeforces Round #Pi (Div. 2)/B.cpp
@@ -0,0 +1,48 @@
+// http://codeforces.com/contest/567/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+#define __ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define foreach(it, x) for (__typeof (x).begin() it = (x).begin(); it != (x).end(); ++it)
+#define all(x) x.begin(),x.end()
+#define D(x) cout << #x " = " << (x) << endl;
+
+template <class T> string toStr(const T &x)
+{ stringstream s; s << x; return s.str(); }
+
+template <class T> int toInt(const T &x)
+{ stringstream s; s << x; int r; s >> r; return r; }
+
+int dx[8] = {-1,-1,-1,0,1,1, 1, 0};
+int dy[8] = {-1, 0, 1,1,1,0,-1,-1};
+
+int main() {
+  int n, a;
+  cin >> n;
+
+  int ans = 0;
+  int cur = 0;
+  char op;
+  map<int, bool> ready;
+
+  for (int i = 0; i < n; ++i) {
+    cin >> op >> a;
+    if (op == '+') {
+      cur ++;
+      ready[a] = true;
+    }
+    else {
+      if (ready[a]) {
+        cur --;
+        ready.erase(a);
+      }
+      else ans ++;
+    }
+
+    ans = max(cur, ans);
+  }
+
+  cout << ans;
+  return 0;
+}
