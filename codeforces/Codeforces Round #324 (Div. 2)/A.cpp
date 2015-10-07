45c3844b5fb2bfa47d5f8d1dc1c4bd48662f6c9a
Jhon Jimenez <jhonb3r@gmail.com>
2015-10-06 22:20:27 -0500

    Added problem 584A from codeforces

---
+++ codeforces/Codeforces Round #324 (Div. 2)/A.cpp
@@ -0,0 +1,35 @@
+// http://codeforces.com/contest/584/problem/A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define __ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+#define foreach(it, x) for (__typeof (x).begin() it = (x).begin(); it != (x).end(); ++it)
+#define all(x) x.begin(),x.end()
+#define D(x) cout << #x " = " << (x) << endl;
+
+template <class T> string toStr(const T &x)
+{ stringstream s; s << x; return s.str(); }
+
+template <class T> int toInt(const T &x)
+{ stringstream s; s << x; int r; s >> r; return r; }
+
+int dx[8] = {-1,-1,-1,0,1,1, 1, 0};
+int dy[8] = {-1, 0, 1,1,1,0,-1,-1};
+
+int main() {
+  int n,t;
+  cin >> n >> t;
+
+  if (n == 1 && t == 10) {
+    cout << -1;
+    return 0;
+  }
+  string res = toStr(t);
+
+  while (res.size() != n) res += "0";
+  cout << res;
+
+
+  return 0;
+}
