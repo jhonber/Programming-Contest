25f4c732b8e7f27f5256a15d3f96dae0125a0fa9
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-20 02:24:08 -0500

    Add problem 1630

--- URI/1630.cpp
+++ URI/1630.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 #define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define endl '\n'
 #define D(x)cout << #x << " = " << (x) << endl;
- 
+
 long long solve(long long a, long long b) {
   if (a < b) swap(a, b);
- 
+
   long long x = 1;
   long long i = 2;
- 
+
   while (i * i <= a) {
     if (a % i == 0) {
       if (b % i == 0) {
@@ -22,17 +22,17 @@ long long solve(long long a, long long b) {
     }
     i ++;
   }
- 
+
   if (a == b) return 4;
   if (a % b == 0) return (a * b) / b;
   return (2 * (a / x) + (2 * (b / x)));
 }
- 
+
 int main() {
   long long x, y;
   while (cin >> x >> y) {
     cout << solve(x, y) << endl;
   }
- 
+
   return 0;
 }
