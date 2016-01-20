2c924ca41fab571b374cbdbca884b86ba173a86c
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-20 18:10:50 -0500

    Add problem 1349

--- URI/1349.cpp
+++ URI/1349.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 #define D(x) cout << #x << " = " << (x) << endl;
- 
+
 int l;
 int mat1[444][444];
 int mat2[444][444];
- 
+
 int check(int f, int c, int a, int b, bool s) {
   int good = 0;
   for (int ii = 0, i = f; ii < l; ++ii, i += a) {
@@ -19,10 +19,10 @@ int check(int f, int c, int a, int b, bool s) {
       }
     }
   }
- 
+
   return 100 * good;
 }
- 
+
 double solve() {
   int ans = 0;
   ans = max(ans, check(0, 0, 1, 1, 0));
@@ -33,10 +33,10 @@ double solve() {
   ans = max(ans, check(l - 1, l - 1, -1, -1, 1));
   ans = max(ans, check(l - 1, 0, -1, 1, 0));
   ans = max(ans, check(l - 1, 0, -1, 1, 1));
- 
+
   return (double)ans / (l * l);
 }
- 
+
 int main() {
   while (cin >> l) {
     if (!l) break;
@@ -45,15 +45,15 @@ int main() {
         cin >> mat1[i][j];
       }
     }
- 
+
     for (int i = 0; i < l; ++i) {
       for (int j = 0; j < l; ++j) {
         cin >> mat2[i][j];
       }
     }
- 
+
     printf("%.2lf\n", solve());
   }
- 
+
   return 0;
 }
