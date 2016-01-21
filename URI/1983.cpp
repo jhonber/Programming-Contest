728af01f7e3d28c0be075f098064729e9248182e
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-21 00:21:27 -0500

    Add problem 1983

--- URI/1983.cpp
+++ URI/1983.cpp
@@ -1,13 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main () {
   int n;
   cin >> n;
- 
+
   pair<int, double> winer;
   winer.second = 0.0;
- 
+
   for (int i = 0; i < n; ++i) {
     double a, b;
     cin >> a >> b;
@@ -16,7 +16,7 @@ int main () {
       winer.second = b;
     }
   }
- 
+
   if (winer.second < 8.0) cout << "Minimum note not reached" << endl;
   else cout << winer.first << endl;
   return 0;
