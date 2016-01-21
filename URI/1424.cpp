ddc8de8963ce409b8b45e1b841fd7ed2c74d7464
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-21 01:21:28 -0500

    Add problem 1424

--- URI/1424.cpp
+++ URI/1424.cpp
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   int n, m;
   while (cin >> n >> m) {
- 
+
   map<int, vector<int> > v;
   for (int i = 0; i < n; ++i) {
     int a;
     cin >> a;
     v[a].push_back(i + 1);
   }
- 
+
   for (int i = 0; i < m; ++i) {
     int a, b;
     cin >> a >> b; a --;
@@ -19,6 +19,6 @@ int main() {
     else cout << "0" << endl;
   }
   }
- 
+
   return 0;
 }
