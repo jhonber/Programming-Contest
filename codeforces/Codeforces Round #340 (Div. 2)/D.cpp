270bb931f5f7c9bda10d248485ce3e6b524fd2e0
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-24 11:53:35 -0500

    Add problem 617D from CF

--- codeforces/Codeforces Round #340 (Div. 2)/D.cpp
+++ codeforces/Codeforces Round #340 (Div. 2)/D.cpp
@@ -7,90 +7,39 @@ using namespace std;
 #define endl '\n'
 #define D(x) cout << #x << " = " << endl;
 
+struct p {
+  int x, y;
+};
+
 int main() {
-  pair<int, int> a,b,c;
+  vector<p> v(3);
+  cin >> v[0].x >> v[0].y >> v[1].x >> v[1].y >> v[2].x >> v[2].y;
 
-  cin >> a.first >> a.second >> b.first >> b.second >> c.first >> c.second;
-
-  int ans = INT_MAX;
-  // x
-  set<int> x1;
-  x1.insert(a.first);
-  x1.insert(b.first);
-  x1.insert(c.first);
-
-  ans = min(ans, (int)x1.size());
-
-  // y
-  set<int> y1;
-  y1.insert(a.second);
-  y1.insert(b.second);
-  y1.insert(c.second);
-
-  ans = min(ans, (int)y1.size());
-
-  if (ans == 1) {
+  if (v[0].x == v[1].x && v[0].x == v[2].x || v[0].y == v[1].y && v[0].y == v[2].y) {
     cout << 1;
     return 0;
   }
 
-  if (a.first == b.first && b.first != c.first) {
-    int ymm = min(a.second, b.second);
-    int ymx = max(a.second, b.second);
+  vector<int> ind = {0, 1, 2};
+  do {
+    if (v[ind[0]].x == v[ind[1]].x && v[ind[0]].x != v[ind[2]].x) {
+      int mx = max(v[ind[0]].y, v[ind[1]].y);
+      int mm = min(v[ind[0]].y, v[ind[1]].y);
+      if (v[ind[2]].y > mm && v[ind[2]].y < mx ) cout << 3;
+      else cout << 2;
+      return 0;
+    }
 
-    if (c.second > ymm && c.second < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
+    if (v[ind[0]].y == v[ind[1]].y && v[ind[0]].y != v[ind[2]].y) {
+      int mx = max(v[ind[0]].x, v[ind[1]].x);
+      int mm = min(v[ind[0]].x, v[ind[1]].x);
 
-  if (a.first == c.first && a.first != b.first) {
-    int ymm = min(a.second, c.second);
-    int ymx = max(a.second, c.second);
-
-    if (b.second > ymm && b.second < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
-
-  if (c.first == b.first && c.first != a.first) {
-    int ymm = min(b.second, c.second);
-    int ymx = max(b.second, c.second);
-
-    if (a.second > ymm && a.second < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
-
-
-  // --
-  if (a.second == b.second && b.second != c.second) {
-    int ymm = min(a.first, b.first);
-    int ymx = max(a.first, b.first);
-
-    if (c.first > ymm && c.first < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
-
-  if (a.second == c.second && a.second != b.second) {
-    int ymm = min(a.first, c.first);
-    int ymx = max(a.first, c.first);
-
-    if (b.first > ymm && b.first < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
-
-  if (c.second == b.second && c.second != a.second) {
-    int ymm = min(b.first, c.first);
-    int ymx = max(b.first, c.first);
-
-    if (a.first > ymm && a.first < ymx) cout << 3;
-    else cout << 2;
-    return 0;
-  }
+      if (v[ind[2]].x > mm && v[ind[2]].x < mx ) cout << 3;
+      else cout << 2;
+      return 0;
+    }
+  } while (next_permutation(ind.begin(), ind.end()));
 
   cout << 3;
-
   return 0;
 }
