98724b5e1635f1e9da4e91e83678935efa39a10d
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-24 23:06:30 -0500

    Add problem 1095

--- URI/1095.cpp
+++ URI/1095.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   int a, b;
   a = 1; b = 60;
- 
+
   while ( b >= 0) {
     cout << "I=" << a << " " << "J=" << b << endl;
     b -= 5;
