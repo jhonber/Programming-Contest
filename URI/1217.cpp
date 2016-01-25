b5606280d4186a9b3f9aef5a5011d446cec757c7
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-25 01:43:19 -0500

    Add problem 1217

--- URI/1217.cpp
+++ URI/1217.cpp
@@ -1,14 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   int n;
   cin >> n;
- 
+
   int cnt = 0, day = 1;
   double sum = 0, sum2 = 0;
   string s;
- 
+
   while (cin >> s) {
     if (isalpha(s[0])) {
       cnt ++;
@@ -26,7 +26,7 @@ int main() {
       }
     }
   }
- 
+
   cout << "day " << day << ": " << cnt << " kg" << endl;
   printf("%.2lf kg by day\n", (double)sum2 / day);
   printf("R$ %.2lf by day\n", sum / day);
