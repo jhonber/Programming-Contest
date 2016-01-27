83ab2b40c339bf23d7c84560f049a8d34a234ef8
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-27 01:02:49 -0500

    Add problem 1583

--- URI/1583.cpp
+++ URI/1583.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int visited[55][55];
 char mat[55][55];
 int x, y;
- 
+
 bool check(int i, int j) {
   return i >= x || i < 0 || j >= y || j < 0;
 }
- 
+
 void dfs(int i, int j) {
   if (!visited[i][j]) {
     mat[i][j] = 'T';
     visited[i][j] = true;
- 
+
     int dx[] = {-1, 0, 1,  0};
     int dy[] = { 0, 1, 0, -1};
     for (int k = 0; k < 4; ++k) {
@@ -25,13 +25,13 @@ void dfs(int i, int j) {
     }
   }
 }
- 
+
 int main() {
- 
+
   while (cin >> x >> y) {
     if (!x && !y) break;
     memset(visited, 0, sizeof visited);
- 
+
     vector<pair<int,int>> pos;
     for (int i = 0; i < x; ++i) {
       for (int j = 0; j < y; ++j) {
@@ -39,11 +39,11 @@ int main() {
         if (mat[i][j] == 'T') pos.push_back({i,j});
       }
     }
- 
+
     for (int i = 0 ; i < pos.size(); ++i) {
       dfs(pos[i].first, pos[i].second);
     }
- 
+
     for (int i = 0; i < x; ++i) {
       for (int j = 0; j < y; ++j) {
         cout << mat[i][j];
@@ -52,6 +52,6 @@ int main() {
     }
     cout << endl;
   }
- 
+
   return 0;
 }
