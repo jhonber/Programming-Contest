6ed447d77a91f21e65bc89d528321fcbb43b67ab
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-29 01:25:26 -0500

    Add problem 1828

--- URI/1828.cpp
+++ URI/1828.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   map< pair<string, string>, string> mapa;
- 
+
   mapa[{"tesoura", "papel"}] = "Bazinga!";
   mapa[{"papel", "pedra"}] = "Bazinga!";
   mapa[{"pedra", "lagarto"}] = "Bazinga!";
@@ -14,20 +14,20 @@ int main() {
   mapa[{"papel", "Spock"}] = "Bazinga!";
   mapa[{"Spock", "pedra"}] = "Bazinga!";
   mapa[{"pedra", "tesoura"}] = "Bazinga!";
- 
+
   int n;
   cin >> n;
- 
+
   for (int i = 0; i < n; ++i) {
     string a, b;
     cin >> a >> b;
- 
+
     cout << "Caso #" << i + 1 << ": ";
     if (a == b) cout << "De novo!";
     else if (mapa[{a,b}] == "Bazinga!") cout << mapa[{a,b}];
     else cout << "Raj trapaceou!";
     cout << endl;
   }
- 
+
   return 0;
 }
