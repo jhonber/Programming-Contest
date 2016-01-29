fc7e15cfa2127a7e3a886bcb15214a22ff5adcfd
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-29 13:23:07 -0500

    Add problem 1041

--- URI/1041.cpp
+++ URI/1041.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-  
+ 
 int main() {
   double x,y;
   cin >> x >> y;
@@ -17,6 +17,6 @@ int main() {
       cout << "Eixo X" << endl;
     }
   }
-  
+ 
   return  0;
 }
