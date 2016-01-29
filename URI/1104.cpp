79a4d04f8ce04064fb44390b743db04766ccb951
Jhon Jimenez <jhonb3r@gmail.com>
2016-01-29 01:59:28 -0500

    Add problem 1104

--- URI/1104.cpp
+++ URI/1104.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
   int a, b;
   while  (cin >> a >> b) {
@@ -11,23 +11,23 @@ int main() {
       cin >> x;
       st1.insert(x);
     }
- 
+
     set<int> st2;
     for (int i = 0; i < b; ++i) {
       cin >> x;
       st2.insert(x);
     }
- 
+
     int t1 = (int)st1.size();
     int t2 = (int)st2.size();
     st1.insert(st2.begin(), st2.end());
     st2.insert(st1.begin(), st1.end());
- 
+
     int d1 = abs(t1 - (int)st1.size());
     int d2 = abs(t2 - (int)st2.size());
- 
+
     cout << min(d1, d2) << endl;
   }
- 
+
   return 0;
 }
