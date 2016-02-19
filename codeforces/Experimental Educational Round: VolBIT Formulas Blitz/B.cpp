d0a8b9177ccd65b6f3ef29f0effa63dcd69f126e
Jhon Jimenez <jhonb3r@gmail.com>
2016-02-18 23:14:49 -0500

    Add problem 630B from CF

---
+++ codeforces/Experimental Educational Round: VolBIT Formulas Blitz/B.cpp
@@ -0,0 +1,12 @@
+// http://codeforces.com/contest/630/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+
+  printf("%.20lf\n", a * pow(1.000000011, b));
+  return 0;
+}
