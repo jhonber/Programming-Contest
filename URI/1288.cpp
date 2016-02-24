e7de1bf256b1e86f0fdc7b9652b88d74d8f770fb
Jhon Jimenez <jhonb3r@gmail.com>
2016-02-24 12:07:18 -0500

    Refactor problem 1288 from URI

--- URI/1288.cpp
+++ URI/1288.cpp
@@ -3,28 +3,25 @@ using namespace std;
 
 #define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define endl '\n'
-#define D(x) cout << #x << " = " << (x) << endl;
 
-int dp[55][555][5];
+int dp[55][333];
 
-int go (int i, bool taken, int w, int &k, vector< pair<int, int> > &v) {
-  if (dp[i][w][taken] != -1) return dp[i][w][taken];
-  if (i == v.size()) return 0;
+int go (int i, int k, int taken, vector< pair<int, int> > &v) {
+  if (dp[i][k] != -1) return dp[i][k];
+  if (i == v.size() || !k) return 0;
+  int ans = 0;
+  if (v[i].second <= k) {
+    ans = max(ans, go (i + 1, k - v[i].second, true, v) + v[i].first);
+  }
 
-  int a = 0;
-  int b = 0;
+  ans = max(ans, go(i + 1, k, taken, v));
 
-  if (w + v[i].second <= k)
-    a += go(i + 1, true, w + v[i].second, k, v) + v[i].first;
-  b += go(i + 1, taken, w, k, v);
-
-  return dp[i][w][taken] = max(a, b);
+  return dp[i][k] = ans;
 }
 
 int main() { IO
   int t;
   cin >> t;
-
   for (int i = 0; i < t; ++i) {
     int n;
     cin >> n;
@@ -40,13 +37,9 @@ int main() { IO
     cin >> k >> r;
 
     memset(dp, -1, sizeof dp);
-    int tot = go (0, 0, 0, k, v);
-
-    if (tot >= r)
-      cout << "Missao completada com sucesso" << endl;
-    else
-      cout << "Falha na missao" << endl;
+    int ans = go (0, k, 0, v);
+    if (ans >= r) cout << "Missao completada com sucesso" << endl;
+    else cout << "Falha na missao" << endl;
   }
-
   return 0;
 }
