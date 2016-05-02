5b4cc9a2849eeb30251227604a60923f92f86f92
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-02 13:46:24 -0500

    Add problem 1197

---
+++ URI/1197.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int a, b;
+
+  while (cin >> a >> b) {
+    cout << (a * b) * 2 << endl;
+  }
+
+  return 0;
+}
