2e622deae49da4ade927cdf085b9af7b4f9bdd2e
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-02 15:59:49 -0500

    Add problem 1957

---
+++ URI/1957.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define d(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int n;
+  while (cin >> n) {
+    cout << hex << uppercase << n << endl;
+  }
+
+  return 0;
+}
