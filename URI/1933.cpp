7e65a7fb33c88cf3f19e10b9e32af5e67e23f464
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-09 15:20:43 -0500

    Add problem 1933

---
+++ URI/1933.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int a, b;
+  while (cin >> a >> b) {
+    cout << max(a, b) << endl;
+  }
+
+  return 0;
+}
