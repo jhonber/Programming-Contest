7e6f35605a29f77131484701b4190de346bd86db
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-09 14:07:50 -0500

    Add problem 1014

---
+++ URI/1014.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+
+  printf("%.3lf km/l\n", a / b);
+  return 0;
+}
