eb090c28a1230324d67fcc429b95425e386383db
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-09 14:45:46 -0500

    Add problem 1564

---
+++ URI/1564.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int n;
+  while (cin >> n) {
+    if (n > 0) cout << "vai ter duas!" << endl;
+    else cout << "vai ter copa!" << endl;
+  }
+
+  return 0;
+}
