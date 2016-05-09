227df9be5e9072e7fa6bd2f568f350a397fed631
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-09 14:48:41 -0500

    Add problem 1387

---
+++ URI/1387.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int a, b;
+  while (cin >> a >> b) {
+    if (a + b == 0) break;
+
+    cout << a + b << endl;
+  }
+
+  return 0;
+}
