b2c7dff0fc5e6c39210ec88f9b76fd9ecd91fb19
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-09 13:57:44 -0500

    Add problem 1004

---
+++ URI/1004.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  cout << "PROD = " << a * b << endl;
+  return 0;
+}
