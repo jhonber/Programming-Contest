801ab3ce0e909b84a96d904a8142c66be7a3770d
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-10 19:17:19 -0500

    Add problem 1959

---
+++ URI/1959.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  long long a, b;
+  cin >> a >> b;
+  cout << a * b << endl;
+  return 0;
+}
