72f691f432d7f4f63b1e204e9965a6065a857ed9
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-22 23:54:10 -0500

    Add problem 1007

---
+++ URI/1007.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " " << (x) << endl;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << "DIFERENCA = " << (a*b - c*d) << endl;
+  return 0;
+}
