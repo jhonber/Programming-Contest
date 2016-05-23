5f107cb841b9f9f815e6f61fa7aade64d10e6069
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-23 00:46:24 -0500

    Add problem 1589

---
+++ URI/1589.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " " << (x) << endl;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int a, b;
+  while (cin >> a >> b) {
+    cout << a + b << endl;
+  }
+  return 0;
+}
