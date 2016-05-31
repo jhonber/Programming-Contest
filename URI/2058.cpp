da4627fc21b78a487e8aab1a74f68818a8376d75
Jhon Jimenez <jhonb3r@gmail.com>
2016-05-31 00:43:39 -0500

    Add problem 2058

---
+++ URI/2058.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl '\n'
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << n - 2 << endl;
+  return 0;
+}
