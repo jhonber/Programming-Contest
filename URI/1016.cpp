3d6ce92f89c38270585501232dc8c67b191d8db6
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 14:38:14 -0500

    Add problem 1016

---
+++ URI/1016.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " < (x) << endl;
+
+int main() {
+  int a;
+  cin >> a;
+
+  cout << 2 * a << " minutos" << endl;
+  return 0;
+}
