88b980a45b0c6dac487365772c58cda867ce6113
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 14:47:26 -0500

    Add problem 1001

---
+++ URI/1001.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " < (x) << endl;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+
+  cout << "X = " << a + b << endl;
+  return 0;
+}
