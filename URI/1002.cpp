9ae44001d73bd90312ea3816cfe17ee928ff34c4
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 18:26:59 -0500

    Add problem 1002

---
+++ URI/1002.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  double r;
+  cin >> r;
+
+  double area = 3.14159 * r * r;
+  printf("A=%.4lf\n", area);
+  return 0;
+}
