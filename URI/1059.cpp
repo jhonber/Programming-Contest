ec2d107db63696cd72910fb9c31da6efd7286781
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 18:25:06 -0500

    Add problem 1059

---
+++ URI/1059.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int i = 2;
+  while (i <= 100) {
+    cout << i << endl;
+    i += 2;
+  }
+
+  return 0;
+}
