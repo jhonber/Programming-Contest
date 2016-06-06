6d4b76e6878cf20ee3c0bf07ae7c3656eb427c11
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 21:02:43 -0500

    Add problem 1924

---
+++ URI/1924.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int n;
+  cin >> n;
+
+  string s;
+  while (n --> 0) {
+    cin >> s;
+  }
+
+  cout << "Ciencia da Computacao" << endl;
+  return 0;
+}
