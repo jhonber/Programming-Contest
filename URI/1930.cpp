23538f44b0077e4917cd5d97fbca383b9482b809
Jhon Jimenez <jhonb3r@gmail.com>
2016-06-05 21:07:52 -0500

    Add problem 1930

---
+++ URI/1930.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+
+  cout << (a + b + c + d) - 3 << endl;
+  return 0;
+}
