1e4a1b55ae0bd4d5817071d9e0cc7420f98891b2
Jhon Jimenez <jhonb3r@gmail.com>
2016-08-10 01:54:52 -0500

    Add problem 2160

---
+++ URI/2160.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  string s;
+  getline(cin, s);
+
+  if (s.size() <= 80) cout << "YES" << endl;
+  else cout << "NO" << endl;
+  return 0;
+}
