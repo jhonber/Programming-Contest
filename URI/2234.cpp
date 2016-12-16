5cfdd3103216428c5897708d4aaf464425b43bcd
Jhon Jimenez <jhonb3r@gmail.com>
2016-12-16 14:21:51 -0500

    Add problem 2234

---
+++ URI/2234.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define D(x) cout << #x << " = " << (x) << endl;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+
+  cout << fixed << setprecision(2) << a / b << endl;
+  return 0;
+}
